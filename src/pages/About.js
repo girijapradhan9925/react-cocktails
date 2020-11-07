a270a82fbbb9efb1b4421be977f0e0e45f292f66
girijapradhan9925 <46650353+girijapradhan9925@users.noreply.github.com>
2020-11-07 22:59:42 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, eos alias
+        consectetur, rem saepe soluta maxime, deleniti nulla architecto
+        reiciendis ex in quos molestias velit neque mollitia nisi voluptatibus
+        inventore accusantium voluptas dignissimos? Tempore velit ipsum officiis
+        exercitationem, et reiciendis.
+      </p>
+    </section>
+  );
+};
+
+export default About;
